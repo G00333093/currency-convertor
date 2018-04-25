148b75e64526a8f2081b498b0b15ce1f8dd51438
G00333093 <G00333093@gmit.ie>
2018-04-25 13:49:47 +0100

    question 2 attempt p1

--- js/convert.js
+++ js/convert.js
@@ -1,9 +1,39 @@
 function euroToPounds(){
 
-  var amount = parseInt(document.getElementById("value1").value);
+  var amount = parseInt(document.getElementById("pounds").value);
 
   var pounds = amount * 0.86;
   var message = amount + ' euros converts to ' + pounds + ' sterling.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
+function euroToDollar(){
+
+  var amount = parseInt(document.getElementById("dollars").value);
+
+  var dollars = amount * 1.06;
+  var message = amount + ' euros converts to ' + dollars + ' dollars.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
+function euroToJapaneseYen(){
+
+  var amount = parseInt(document.getElementById("japaneseyen").value);
+
+  var japaneseyen = amount * 121.12;
+  var message = amount + ' euros converts to ' + japaneseyen + ' japaneseyen.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+
+}
+function euroToBitcoin(){
+
+  var amount = parseInt(document.getElementById("bitcoin").value);
+
+  var bitcoin = amount * 0.00082;
+  var message = amount + ' euros converts to ' + bitcoin + ' bitcoin.';
   console.log(message);
   document.getElementById("resultsentence").innerHTML = message;
 
